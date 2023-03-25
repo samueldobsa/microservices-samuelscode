625ddd324dae285f2264f78e8d38dbba46f52933
Samuel Dobsa <samueldobsa@192.168.1.82>
2023-03-26 00:48:14 +0100

    Initial commit

---
+++ src/main/java/com/example/microservicessamuelscode/MicroservicesSamuelscodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicessamuelscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesSamuelscodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesSamuelscodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/microservicessamuelscode/MicroservicesSamuelscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicessamuelscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesSamuelscodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
